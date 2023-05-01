15655f520ace96f30b1562292c6000817e668e05
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-05-01 17:27:37 -0600

    feat: Add 404 and error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { Request, Response, NextFunction } from "express";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -14,6 +15,19 @@ app.get("/api/whoami", (req, res) => {
     language: req.headers["accept-language"],
     software: req.headers["user-agent"],
   });
+});
+
+// eslint-disable-next-line
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .json({ message: "This is not the route you're looking for." });
+});
+
+// eslint-disable-next-line
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.message);
+  res.status(500).json({ error: "Something broke" });
 });
 
 export default app;
